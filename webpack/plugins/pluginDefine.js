fbc871a57ce96b586e6f5ab12c250511f12fb7a1
jaredmcgrath <jared@pacefactory.com>
2020-11-15 18:37:25 -0500

    Add environment variables from file support

--- webpack/plugins/pluginDefine.js
+++ webpack/plugins/pluginDefine.js
@@ -1,5 +1,6 @@
-import {DefinePlugin} from 'webpack';
+import {DefinePlugin} from "webpack";
 
+import {envVars} from "../utils/env";
 /**
  * Created by: Andrey Polyakov (andrey@polyakov.im)
  * @example
@@ -7,15 +8,21 @@ import {DefinePlugin} from 'webpack';
  *     isProd: true
  * }
  */
-import {isDev, isDevServer, isProd, mode} from '../utils/env';
+import {isDev, isDevServer, isProd, mode} from "../utils/env";
 
 const config = {
-    'process.env': {
-        NODE_ENV: JSON.stringify(mode),
-    },
-    IS_PROD: isProd,
-    IS_DEV: isDev,
-    IS_DEV_SERVER: isDevServer,
+  "process.env": {
+    NODE_ENV: JSON.stringify(mode),
+  },
+  IS_PROD: isProd,
+  IS_DEV: isDev,
+  IS_DEV_SERVER: isDevServer,
 };
+
+if (envVars) {
+  Object.keys(envVars).forEach((key) => {
+    config[`process.env.${key}`] = JSON.stringify(envVars[key]);
+  });
+}
 
 export const definePlugin = new DefinePlugin(config);
--- webpack/utils/env.js
+++ webpack/utils/env.js
@@ -1,14 +1,18 @@
 /**
  * Created by: Andrey Polyakov (andrey@polyakov.im)
  */
-import {join} from 'path';
+import {join} from "path";
 
-import {parseArguments} from './helpers';
+import dotenv from "dotenv";
+
+import {parseArguments} from "./helpers";
 
 const parsedArguments = parseArguments();
-export const mode = parsedArguments.mode ?? 'production';
+export const mode = parsedArguments.mode ?? "production";
 export const isDevServer = parsedArguments.isDevServer ?? false;
-export const isProd = mode === 'production';
+export const isProd = mode === "production";
 export const isDev = !isProd;
-export const rootDir = join(__dirname, '../../');
-export const webpackDir = join(__dirname, '../');
+export const rootDir = join(__dirname, "../../");
+export const webpackDir = join(__dirname, "../");
+export const envVars = dotenv.config({path: join(rootDir, "configs/.env")})
+  .parsed;
